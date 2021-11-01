849caa8d26743b5c6a9c20ff97871a15a7026ff0
suvigyabhardwaj <suvigya0797@gmail.com>
2021-11-01 16:02:10 +0530

    commit

--- deploy/fake_news_detection/fake_news_detection/settings.py
+++ deploy/fake_news_detection/fake_news_detection/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-f_t2h8u!7c)ylz3k$_=0**%z*8cpe9sv=pr)!2ns(a3^r8w*k8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["fakenewsdetection.herokuapp.com"]
 
 
 # Application definition
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -122,6 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT= BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
@@ -130,5 +132,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MODEL_DIR=os.path.join(BASE_DIR,'model')
 
-loaded_model=pickle.load(open(MODEL_DIR+'\\model.pkl', 'rb'))
-dataframe=pd.read_csv(MODEL_DIR+'\\news.csv')
+loaded_model=pickle.load(open(MODEL_DIR+'//model.pkl', 'rb'))
+dataframe=pd.read_csv(MODEL_DIR+'//news.csv')
